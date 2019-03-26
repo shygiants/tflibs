1b5d2e181ba1f0f6d4d00a1b43fedd5e44ee1e67
Sanghoon Yoon <shygiants@gmail.com>
2019-03-26 13:33:41 +0900

    Bug fixed

--- tflibs/ops/random.py
+++ tflibs/ops/random.py
@@ -8,7 +8,9 @@ def randomly_pick_op(ops: list):
         rand_var = tf.random_uniform(()) * len(ops)
 
         def picked(inputs: tf.Tensor):
-            pred_fn_pairs = dict([(tf.less(rand_var, i + 1), lambda: op(inputs)) for i, op in enumerate(ops)])
+            pred_fn_pairs = dict(
+                [(tf.logical_and(tf.greater_equal(rand_var, i), tf.less(rand_var, i + 1)), lambda: op(inputs)) for i, op
+                 in enumerate(ops)])
 
             return tf.case(pred_fn_pairs, exclusive=True)
 
